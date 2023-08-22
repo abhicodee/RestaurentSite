da001e4adef082b4d1d7a7f53a60cd61711f4fe4
abhicodee <abhishekssrivastav14@gmail.com>
2023-08-22 17:23:53 +0530

    dialog box

--- src/components/Product/Dialogue.js
+++ src/components/Product/Dialogue.js
@@ -7,7 +7,7 @@ const Dialogue = ( { isOpen, onClose }) => {
    <>
       <div className="full-screen-dialog">
       <div className="dialog-content">
-        <h4>It will make food ordering easier for customers. 
+        <h4 className='dicontent'>It will make food ordering easier for customers. 
             It ends up saving much time as 
             it saves the time that a mediator takes to pen down the entire order.
              It is also easy to track the order delivery over a mobile application.</h4>
